fd973b6948975048ddeb68008b18e4bc907b8582
Naphat Phoruang <99393112+guide1121@users.noreply.github.com>
2025-02-06 23:43:27 +0700

    add popup

--- animation.js
+++ animation.js
@@ -63,4 +63,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     observer.observe(section);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const section = document.querySelector(".section-impact");
 
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    section.classList.add("show");
+                }
+            });
+        },
+        { threshold: 0.5 }
+    );
+
+    observer.observe(section);
+});
+
+// ฟังก์ชันเปิด Popup พร้อมโหลดรูปที่คลิก
+function openPopup(imgElement) {
+    let popup = document.getElementById("imagePopup");
+    let popupImg = document.getElementById("popupImage");
+
+    popup.style.display = "flex";  // แสดง popup
+    popupImg.src = imgElement.src; // เปลี่ยนรูปเป็นรูปที่ถูกคลิก
+}
+
+// ฟังก์ชันปิด Popup
+function closePopup() {
+    document.getElementById("imagePopup").style.display = "none";
+}
