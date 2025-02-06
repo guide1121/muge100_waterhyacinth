ed68693864ed9bd756b44fadbcc8f527fd6199f5
Naphat Phoruang <99393112+guide1121@users.noreply.github.com>
2025-02-06 23:56:58 +0700

    add logo

--- ่navbar_scroll.js
+++ ่navbar_scroll.js
@@ -15,3 +15,17 @@ window.addEventListener('scroll', () => {
         
     }
 });
+
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector(".header");
+    let logo = document.getElementById("logo");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+        logo.src = "images/logo-black.png"; // เปลี่ยนเป็นโลโก้สีดำ
+    } else {
+        header.classList.remove("scrolled");
+        logo.src = "images/logo-white.png"; // กลับเป็นโลโก้สีขาว
+    }
+});
